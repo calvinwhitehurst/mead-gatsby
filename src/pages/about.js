069b74affceb7f2a5f6f39ac7f2153c6cdf49cf3
Calvin Whitehurst <calvin@calvinwhitehurst.com>
2021-07-11 16:16:26 -0700

    added links to index, about, and contact

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const AboutPage = () =>{
     return(
         <div>
             <h1>About</h1>
             <p>This is an example about page</p>
+            <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
+
         </div>
     )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const ContactPage = () =>{
     return(
         <div>
             <h1>Contact</h1>
             <p>Drop me a line</p>
+            <p>Please visit my <Link to="https://twitter.com/CalvinWhitehurs">Twitter Profile</Link></p>
         </div>
     )
 }
